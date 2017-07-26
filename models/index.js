9879f71603ce3cbe83fa7600dccd6eab5fb9e7b5
Sam Ascher <smascher@Sams-MacBook-Air.local>
2017-07-26 16:33:10 -0600

    got profile + RH hard coding down

--- models/index.js
+++ models/index.js
@@ -4,5 +4,4 @@ mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTI
                   process.env.MONGOHQ_URL || 
                   "mongodb://localhost/personal-api");
 
-// module.exports.Campsite = require("./campsite.js.example");
-
+module.exports.Campsite = require("./campsite.js.example");
--- server.js
+++ server.js
@@ -12,7 +12,76 @@ app.use(bodyParser.json());
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
+
+/******************
+Some hardcoded data
+******************/
+
+var profile = {
+  id: _1,
+  name: "Sam Ascher",
+  current_city: "Denver",
+  github_link: "https://github.com/samascher",
+  github_profile_image: "https://github.com/account",
+  favorite_drinks: [
+    {
+      name: "riesling",
+      type: "wine",
+      temperature: "cold"
+    },
+    {
+      name: "sauvignon blanc",
+      type: "wine",
+      temperature: "very cold"
+    },
+    {
+      name: "lemonade",
+      type: "flavored water",
+      temperature: "semi-cold"
+    },
+    {
+      name: "la croix cran-raspberry",
+      type: "sparkling water",
+      temperature: "room-temp",
+    },
+  ]
+};
+
+var realHousewives = [
+  {
+    id: _1,
+    name: "Dorinda",
+    location: "New York City",
+    flaw: "Slurring, crazy drunk",
+    number_of_husbands: 2,
+    current_status: "active"
+  },
+  {
+      id: _2,
+    name: "Tamra",
+    location: "Orange County",
+    flaw: "Anger issues",
+    number_of_husbands: 2,
+    current_status: "active"
+  },
+  {    
+    id: _3,
+    name: "Mama Elsa",
+    location: "Miami",
+    flaw: "botched plastic surgeries",
+    number_of_husbands: 3,
+    current_status: "deceased"
+  },
+  {
+    id: _4,
+    name: "Lisa Vanderpump",
+    location: "Beverly Hills",
+    flaw: "Stuck-up old bitch",
+    number_of_husbands: 1,
+    current_status: "active"
+  }
+],
 
 /**********
  * ROUTES *
@@ -39,14 +108,20 @@ app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    message: "Hey what's up hello!? Welcome to Sam's personal api.. here's what you need to know!",
+    documentation_url: "https://github.com/samascher/express-personal-api", // Changed -- need to add documentation
+    base_url: "https://pacific-scrubland-15691.herokuapp.com/", // Changed
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Some basic information about me!"},
+      {method: "POST", path: "/api/campsites", description: "My favorite Real Housewives"}
     ]
+  });
+});
+
+app.get('/api/profile', function (req, res){
+  db.Profile.find({}, function (err, profile){
+    res.json(profile);
   });
 });
 
